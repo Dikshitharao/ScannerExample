bb40d0db1f66a0d09ecc732714b43bce7a6b3b1f
Dikshitha Rao <dikshitharao48798@gmail.com>
2021-06-13 16:11:06 +0530

    Add files via upload

---
+++ ScannerExample/src/com/blogger/scanner/ScannerExample.java
@@ -0,0 +1,20 @@
+package com.blogger.scanner;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+	      int a = sc.nextInt();
+	      int b = sc.nextInt();
+	      System.out.println(a+b);
+	      String s = sc.next();
+	       System.out.println(s);
+	      char c = sc.next().charAt(0);
+	      System.out.println(c);
+
+	}
+
+}
